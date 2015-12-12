a1e99c2295e98dc08c80a073316d90f5a02e41ee
Adrian Astley <adastley@gmail.com>
2015-12-12 01:25:04 -0600

    Add AABB and bounding sphere to Mesh

--- Mesh.cs
+++ Mesh.cs
@@ -1,9 +1,14 @@
 using System.Collections.Generic;
+using Dargon.Scene.Api.Util;
 
 namespace Dargon.Scene.Api {
    public class Mesh {
       public IReadOnlyList<Vertex> Vertices { get; set; }
       public IReadOnlyList<ushort> Indices { get; set; }
+
+      public Vector4 BoundingSphere { get; set; }
+      public AABB AABB { get; set; }
+
       public string TexturePath { get; set; }
    }
 }
